93411a51515a972a15fca2cca88c3fac368cd81d
Katya <laptop@Laptops-MacBook-Air.local>
2019-11-18 19:05:45 +0600

    bad things happens

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ from flask_login import LoginManager
 from flask_script import Manager
 
 
-app = Flask(__name__, template_folder='/Users/laptop/PycharmProjects/untitled2/templates',
-            static_folder='/Users/laptop/PycharmProjects/untitled2/static')
+app = Flask(__name__,
+            # template_folder='/Users/laptop/PycharmProjects/untitled2/templates',
+            # static_folder='/Users/laptop/PycharmProjects/untitled2/static'
+            )
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://localhost:5432/site'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
